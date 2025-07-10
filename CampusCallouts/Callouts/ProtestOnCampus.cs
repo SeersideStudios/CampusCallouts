8c4699745ca0f1962cf43f140187e0155ec22a3a
SeersideStudios <seersidestudios@gmail.com>
2025-07-10 03:57:56 -0500

    Finishing Fixes

--- CampusCallouts/Callouts/ProtestOnCampus.cs
+++ CampusCallouts/Callouts/ProtestOnCampus.cs
@@ -38,6 +38,27 @@ namespace CampusCallouts.Callouts
         private Vector3 ProtestLocation = new Vector3(-1650f, 215f, 60.5f);
         private Vector3 DeanDestination = new Vector3(-1708.795f, 77.58182f, 65.76763f);
 
+        Vector3[] protestorPositions = new Vector3[]
+        {
+            new Vector3(-1609.974f, 233.0879f, 59.72863f),
+            new Vector3(-1606.334f, 231.8275f, 59.46855f),
+            new Vector3(-1602.697f, 233.2524f, 59.32865f),
+            new Vector3(-1604.24f, 236.5777f, 59.37085f),
+            new Vector3(-1605.595f, 239.4767f, 59.37411f),
+            new Vector3(-1602.211f, 241.1233f, 59.32285f),
+            new Vector3(-1600.589f, 237.7915f, 59.27518f),
+            new Vector3(-1598.847f, 234.2126f, 59.23326f),
+            new Vector3(-1595.882f, 235.6555f, 59.17066f),
+            new Vector3(-1597.23f, 238.5398f, 59.22754f)
+        };
+
+        float[] headings = new float[]
+        {
+            117.0332f, 115.4989f, 115.4989f, 115.4989f, 115.4989f,
+            115.4989f, 115.4989f, 115.4989f, 115.4989f, 115.4989f
+        };
+
+
         private Random rand = new Random();
         private int scenarioOption;
         private bool OnScene = false;
@@ -45,7 +66,12 @@ namespace CampusCallouts.Callouts
         private bool EscortStarted = false;
 
         private WindowsMediaPlayer protestPlayer;
-        private string protestMusicPath = @"Plugins\LSPDFR\audio\scanner\CampusCallouts - Audio\Protest\CC_PROTEST_AUDIO.mp3";
+        private string protestMusicPath = System.IO.Path.Combine(
+            AppDomain.CurrentDomain.BaseDirectory,
+            @"LSPDFR\Audio\Scanner\CampusCallouts - Audio\Protest\CC_PROTEST_AUDIO.mp3"
+        );
+
+
 
 
         public override bool OnBeforeCalloutDisplayed()
@@ -58,9 +84,13 @@ namespace CampusCallouts.Callouts
             CalloutAdvisory = "ULSA staff report a large group of students protesting. Situation may escalate.";
 
             if (Settings.UseBluelineAudio)
+            {
                 LSPD_First_Response.Mod.API.Functions.PlayScannerAudioUsingPosition("POSSIBLE_DISTURBANCE IN_OR_ON_POSITION", CalloutPosition);
+            }
             else
+            {
                 LSPD_First_Response.Mod.API.Functions.PlayScannerAudioUsingPosition("CC_WE_HAVE CC_POSSIBLE_DISTURBANCE IN_OR_ON_POSITION", CalloutPosition);
+            }
 
             LSPD_First_Response.Mod.API.Functions.PlayScannerAudio("UNITS_RESPOND_CODE_03_02");
 
@@ -74,28 +104,35 @@ namespace CampusCallouts.Callouts
 
             // Fixed coordinates
             Vector3 protestorCenter = new Vector3(-1610.732f, 231.8715f, 59.78891f);
-            Vector3 deanPos = new Vector3(-1622.43f, 227.5778f, 60.26468f);
-            float deanHeading = 289.4158f;
+            Vector3 deanPos = new Vector3(-1619.931f, 228.1242f, 60.18069f);
+            float deanHeading = 296.7391f;
 
             // Spawn Protestors
-            int totalProtestors = rand.Next(10, 21); // 10 to 20 protestors
-            for (int i = 0; i < totalProtestors; i++)
+            Rage.Native.NativeFunction.CallByName<uint>("REQUEST_ANIM_DICT", "missheistdockssetup1leadinoutig_1");
+            while (!Rage.Native.NativeFunction.CallByName<bool>("HAS_ANIM_DICT_LOADED", "missheistdockssetup1leadinoutig_1"))
             {
-                Vector3 pos = protestorCenter.Around2D(4f); // 4-meter radius
-                string modelName = protestorModels[rand.Next(protestorModels.Count)];
+                GameFiber.Yield();
+            }
 
-                Ped protestor = new Ped(modelName, pos, 108.7844f);
+            // Spawn protestors at fixed positions
+            for (int i = 0; i < 10; i++)
+            {
+                string modelName = protestorModels[rand.Next(protestorModels.Count)];
+                Ped protestor = new Ped(modelName, protestorPositions[i], headings[i]);
+
                 protestor.MakePersistent();
                 protestor.BlockPermanentEvents = true;
+
                 protestor.Tasks.PlayAnimation("missheistdockssetup1leadinoutig_1", "lsdh_ig_1_argue_les", 1f, AnimationFlags.Loop);
                 Protestors.Add(protestor);
                 GameFiber.Yield();
             }
 
+
             // Spawn Dean and Teachers at precise positions and heading
             Dean = new Ped("A_M_M_Business_01", deanPos, deanHeading);
-            Teacher1 = new Ped("A_M_Y_Business_01", new Vector3(-1623.5f, 228.0f, 60.26468f), deanHeading);
-            Teacher2 = new Ped("A_F_Y_Business_03", new Vector3(-1621.2f, 227.0f, 60.26468f), deanHeading);
+            Teacher1 = new Ped("A_M_Y_Business_01",  new Vector3(-1621.077f, 230.687f, 60.23051f), 293.6463f);
+            Teacher2 = new Ped("A_F_Y_Business_03", new Vector3(-1618.666f, 225.3496f, 60.11632f), 297.5822f);
 
             foreach (Ped staff in new[] { Dean, Teacher1, Teacher2 })
             {
@@ -134,7 +171,7 @@ namespace CampusCallouts.Callouts
         {
             base.Process();
 
-            if (!OnScene && Game.LocalPlayer.Character.Position.DistanceTo(Dean) < 15f)
+            if (!OnScene && Game.LocalPlayer.Character.Position.DistanceTo(Dean) < 30f)
             {
                 OnScene = true;
                 Dean.Face(Game.LocalPlayer.Character);
@@ -142,6 +179,7 @@ namespace CampusCallouts.Callouts
                 Game.DisplayHelp("Press ~y~" + Settings.DialogueKey + "~w~ to speak to the Dean.");
 
                 // Play custom protest music
+                Game.LogTrivial("CampusCallouts - Checking protest audio at: " + protestMusicPath);
                 if (System.IO.File.Exists(protestMusicPath))
                 {
                     protestPlayer = new WindowsMediaPlayer();
@@ -162,6 +200,28 @@ namespace CampusCallouts.Callouts
                 RunDialogue();
                 GameFiber.Sleep(300); // debounce
             }
+
+            if (EscortStarted && Dean.Exists() && Dean.Position.DistanceTo(DeanDestination) > 15f && Hostiles.Count == 0)
+            {
+                int attackers = rand.Next(1, 8); 
+                Game.LogTrivial($"CampusCallouts - Protest - {attackers} protestors are attacking the Dean!");
+
+                for (int i = 0; i < attackers; i++)
+                {
+                    Ped attacker = Protestors[rand.Next(Protestors.Count)];
+
+                    // Ensure no duplicates
+                    if (!Hostiles.Contains(attacker) && attacker.Exists() && !attacker.IsDead)
+                    {
+                        attacker.BlockPermanentEvents = false;
+                        attacker.Tasks.FightAgainst(Dean);
+                        Hostiles.Add(attacker);
+                    }
+                }
+
+                Game.DisplayNotification("Some protestors are trying to harm the Dean!");
+            }
+
 
             if (EscortStarted && Game.LocalPlayer.Character.Position.DistanceTo(DeanDestination) < 3f)
             {
